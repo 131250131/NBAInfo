2e0051d6b83c01b725620d367ded8d375a77f11e
david941211 <657834200@qq.com>
2015-03-09 17:05:25 +0800

    先更新一下

--- src/com/main/MatchFileReader.java
+++ src/com/main/MatchFileReader.java
@@ -39,8 +39,15 @@ public class MatchFileReader {
         read.close();
 	}
 	
-	String leftTeamName, rightTeamName;
-	int leftTeamScore, rightTeamScore;
+	String leftTeamShortName, rightTeamShortName;
+	int leftTeamScore = 0, rightTeamScore = 0;
+	int leftTeamTime = 0, rightTeamTime = 0;
+	
+	int counter = 0;
+	String[] playerName = new String[24];
+	int[] playerTime = new int[24];
+	boolean[] playerIsStart = new boolean[24];
+	
 	boolean isLeft = true;
 	
 	private void operateMatchTxt(String lineTxt, int lineNumber){
@@ -48,22 +55,44 @@ public class MatchFileReader {
 			String[] teamInfoStage1 = lineTxt.split(";");
 			
 			String[] teamInfoStage2 = teamInfoStage1[1].split("-");
-			leftTeamName = teamInfoStage2[0];
-			rightTeamName = teamInfoStage2[1];
+			leftTeamShortName = teamInfoStage2[0];
+			rightTeamShortName = teamInfoStage2[1];
 			
 			String[] teamInfoStage3 = teamInfoStage1[2].split("-");			
 			leftTeamScore = Integer.parseInt(teamInfoStage3[0]);
 			rightTeamScore = Integer.parseInt(teamInfoStage3[1]);
 		}
 		
-		if(lineNumber > 3 && lineTxt.length() == 3){
+		if(lineNumber > 2 && lineTxt.length() == 3){
 			isLeft = false;
 		}
 		
-		if(lineNumber > 3 && lineTxt.length() != 3){
+		if(lineNumber > 2 && lineTxt.length() != 3){
+			//System.out.println(lineTxt + isLeft);
 			String[] playerInfo = lineTxt.split(";");
 			
+			playerName[counter] = playerInfo[0];
+			
+			if(playerInfo[1].length() > 0){
+				playerIsStart[counter] = true;
+			}else{
+				playerIsStart[counter] = false;
+			}
+			
+			playerTime[counter] = playTimeTransform(playerInfo[2]);
+			if(isLeft){
+				leftTeamTime = leftTeamTime + playerTime[counter];
+			}else{
+				rightTeamTime = rightTeamTime + playerTime[counter];
+			}
+			
+			counter++;
 		}
 	}
+	
+	private int playTimeTransform(String time){
+	  	String[] array = time.split(":");
+	  	return Integer.parseInt(array[0]) * 60 + Integer.parseInt(array[1]);//单位秒
+		}
 	
 }
--- src/com/main/Player.java
+++ src/com/main/Player.java
@@ -25,6 +25,42 @@ public class Player implements java.io.Serializable{
 		school = _playerSchool;
 		playerID = _playerID;
 		
+		playerAttends = 0;
+		startTimes = 0;
+		playerAssists = 0;
+		playerPlayTime = 0;
+		playerFG = 0;
+		playerFGTry = 0; 
+		player3FG = 0;
+		player3FGTry = 0;
+		playerFTG = 0;
+		playerFTGTry = 0;
+		playerOffenceRebounds = 0;
+		playerDeffenceRebounds = 0;
+		playerTotalRebounds = 0;
+		playerSteals = 0;
+		playerBlocks = 0;		
+		playerTurnovers = 0;
+		playerFouls = 0;
+		playerScores = 0;
+		teammatePlayTime = 0;
+	    allTeamRebounds = 0;
+		allEnemyRebounds = 0;
+		o_allTeamRebounds = 0;
+		o_allEnemyRebounds = 0;
+		d_allTeamRebounds = 0;
+		d_allEnemyRebounds = 0;
+		allTeamFT = 0;
+		allTeamShoots = 0;
+		allEnemyShoots = 0;
+		allTeamFTShoots = 0;
+		allTeamTurnover = 0;
+		allEnemyOffenceTimes = 0;
+	}
+	
+	public void updatePlayer(){
+		playerAttends++;
+		
 	}
 	
 	//球员类， 用序列号进行保存
@@ -79,11 +115,9 @@ public class Player implements java.io.Serializable{
 	//15.队员投篮命中数 (*****要传*****)
 		int playerFG; 
 		
-	//16.a队员投篮出手数 (*****要传*****)
+	//16.队员投篮出手数 (*****要传*****)
 		int playerFGTry; 
-	//16.b队员2分球出手数 (*****要传*****)
-		int player2FGTry;
-		
+			
 	//17.队员三分命中数 (*****要传*****)
 		int player3FG;
 		
@@ -120,98 +154,98 @@ public class Player implements java.io.Serializable{
 	//28.队员总得分数 (*****要传*****)
 		int playerScores; 
 		
-	//29.队员平均助攻数
-		private double aver_playerAssists ;
-		
-	//30.队员平均在场时间
-		private double aver_playerPlayTime  ;
-		
-	//31.队员平均投篮命中数
-		private double aver_playerFG  ;
-		
-	//32.队员平均投篮出手数
-		private double aver_playerFGTry ;
-		
-	//33.队员平均三分命中数
-		private double aver_player3FG ;
-				
-	//34.队员平均三分出手数
-		private double aver_player3FGTry ;
-		
-	//35.队员平均罚球命中数
-		private double aver_playerFTG ;
-		
-	//36.队员平均罚球出手数
-		private double aver_playerFTGTry ;
-		
-	//37.队员平均进攻篮板数
-		private double aver_playerOffenceRebounds ;
-		
-	//38.队员平均防守篮板数
-		private double aver_playerDeffenceRebounds ;
-		
-	//39.队员平均总篮板数
-		private double aver_playerTotalRebounds ;
-		
-	//40.队员平均抢断数
-		private double aver_playerSteals ;
-		
-	//41.队员平均盖帽数
-		private double aver_playerBlocks ;
-		
-	//42.队员平均失误数
-		private double aver_playerTurnovers ;
-		
-	//43.队员平均犯规数
-		private double aver_playerFouls ;
-		
-	//44.队员平均得分
-		private double aver_playerScores ;
-		
-	//45.队员投篮命中率
-		private double playerFGP ;
-		
-	//46.队员三分命中率
-		private double player3FGP ;
-		
-	//47.队员罚球命中率	
-		private double playerFTGP ;
-		
-	//48.队员效率(PER)
-		private double playerPER ;
-		
-	//49.队员GmSc效率值
-		private double playerGmScER ;
-		
-	//50.队员真实命中率
-		private double playerTSP ;
-		
-	//51.队员投篮效率
-		private double playerSER ;
-		
-	//52.队员篮板率
-		private double RR;
-		
-	//53.进攻篮板率
-		private double o_RR;
-		
-	//54.防守篮板率
-		private double d_RR;
-		
-	//55.助攻率
-		private double AR;
-		
-	//56.抢断率
-		private double SR;
-		
-	//57.盖帽率
-		private double BR;
-		
-	//58.失误率
-		private double TR;
-		
-	//59.使用率
-		private double UR;
+//	//29.队员平均助攻数
+//		private double aver_playerAssists ;
+//		
+//	//30.队员平均在场时间
+//		private double aver_playerPlayTime  ;
+//		
+//	//31.队员平均投篮命中数
+//		private double aver_playerFG  ;
+//		
+//	//32.队员平均投篮出手数
+//		private double aver_playerFGTry ;
+//		
+//	//33.队员平均三分命中数
+//		private double aver_player3FG ;
+//				
+//	//34.队员平均三分出手数
+//		private double aver_player3FGTry ;
+//		
+//	//35.队员平均罚球命中数
+//		private double aver_playerFTG ;
+//		
+//	//36.队员平均罚球出手数
+//		private double aver_playerFTGTry ;
+//		
+//	//37.队员平均进攻篮板数
+//		private double aver_playerOffenceRebounds ;
+//		
+//	//38.队员平均防守篮板数
+//		private double aver_playerDeffenceRebounds ;
+//		
+//	//39.队员平均总篮板数
+//		private double aver_playerTotalRebounds ;
+//		
+//	//40.队员平均抢断数
+//		private double aver_playerSteals ;
+//		
+//	//41.队员平均盖帽数
+//		private double aver_playerBlocks ;
+//		
+//	//42.队员平均失误数
+//		private double aver_playerTurnovers ;
+//		
+//	//43.队员平均犯规数
+//		private double aver_playerFouls ;
+//		
+//	//44.队员平均得分
+//		private double aver_playerScores ;
+//		
+//	//45.队员投篮命中率
+//		private double playerFGP ;
+//		
+//	//46.队员三分命中率
+//		private double player3FGP ;
+//		
+//	//47.队员罚球命中率	
+//		private double playerFTGP ;
+//		
+//	//48.队员效率(PER)
+//		private double playerPER ;
+//		
+//	//49.队员GmSc效率值
+//		private double playerGmScER ;
+//		
+//	//50.队员真实命中率
+//		private double playerTSP ;
+//		
+//	//51.队员投篮效率
+//		private double playerSER ;
+//		
+//	//52.队员篮板率
+//		private double RR;
+//		
+//	//53.进攻篮板率
+//		private double o_RR;
+//		
+//	//54.防守篮板率
+//		private double d_RR;
+//		
+//	//55.助攻率
+//		private double AR;
+//		
+//	//56.抢断率
+//		private double SR;
+//		
+//	//57.盖帽率
+//		private double BR;
+//		
+//	//58.失误率
+//		private double TR;
+//		
+//	//59.使用率
+//		private double UR;
 		
 		
 	//60.球员队友出场时间 (*****要传*****)
@@ -375,7 +409,7 @@ public class Player implements java.io.Serializable{
 		}
 
 		public double getTR() {
-			return playerTurnovers/(player2FGTry+0.44*playerFTGTry+playerTurnovers);
+			return playerTurnovers/(playerFGTry - player3FGTry + 0.44 * playerFTGTry+playerTurnovers);
 		}
 
 		public double getUR() {
