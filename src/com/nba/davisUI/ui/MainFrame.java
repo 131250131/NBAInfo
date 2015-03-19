209803d32e2b483a3622faa72798aba6f47fb9ea
david941211 <657834200@qq.com>
2015-03-19 20:50:01 +0800

    update

--- src/com/nba/davisUI/ui/MainFrame.java
+++ src/com/nba/davisUI/ui/MainFrame.java
@@ -3,9 +3,16 @@ package com.nba.davisUI.ui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import com.nba.davisUI.myUI.ImageBin;
 import com.nba.davisUI.myUI.MyFrame;
 
 
@@ -17,11 +24,60 @@ public class MainFrame extends MyFrame implements ActionListener{
 	LoadingPanel loadingPanel;
 	PlayerTablePanel playerTablePanel;
 	Index index;
+	JLabel exitButton, mimiButton;
+	JButton exitBt, miniBt;
 	
-	public static JButton openPlayerTablePanel;
+	public static JButton openPlayerTablePanel, refresh;
 	public MainFrame(){
 		
 		this.setLayout(null);
+		
+		exitButton = new JLabel(ImageBin.getImage("exit1"));
+		exitButton.setBounds(0, 0, 1280, 720);
+		exitButton.setVisible(true);
+		this.add(exitButton);
+		
+		mimiButton = new JLabel(ImageBin.getImage("mini1"));
+		mimiButton.setBounds(0, 0, 1280, 720);
+		mimiButton.setVisible(true);
+		this.add(mimiButton);
+		
+		exitBt = new JButton();
+		exitBt.setBounds(1280 - 28, 0, 28, 33);
+		exitBt.setOpaque(false);
+		exitBt.setBackground(null);
+		exitBt.setBorder(BorderFactory.createEmptyBorder());
+		exitBt.setContentAreaFilled(false);
+		
+		this.add(exitBt);
+		exitBt.addActionListener(this);
+		exitBt.addMouseListener(new MouseAdapter(){
+			public void mouseEntered(MouseEvent arg0) {
+				exitButton.setIcon(ImageBin.getImage("exit2"));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				exitButton.setIcon(ImageBin.getImage("exit1"));
+			}
+			}); 
+		
+		miniBt = new JButton();
+		miniBt.setBounds(1280 - 25 - 28, 0, 25, 33);
+		miniBt.setOpaque(false);
+		miniBt.setBackground(null);
+		miniBt.setBorder(BorderFactory.createEmptyBorder());
+		miniBt.setContentAreaFilled(false);
+		
+		this.add(miniBt);
+		miniBt.addActionListener(this);
+		miniBt.addMouseListener(new MouseAdapter(){
+			public void mouseEntered(MouseEvent arg0) {
+				mimiButton.setIcon(ImageBin.getImage("mini2"));
+			}
+			public void mouseExited(MouseEvent arg0) {
+				mimiButton.setIcon(ImageBin.getImage("mini1"));
+			}
+			}); 
+		
 		
 		loadingPanel = new LoadingPanel();	
 		this.add(loadingPanel);
@@ -29,6 +85,10 @@ public class MainFrame extends MyFrame implements ActionListener{
 		openPlayerTablePanel= new JButton();
 		openPlayerTablePanel.addActionListener(this);
 		this.add(openPlayerTablePanel);
+		
+		refresh = new JButton();
+		refresh.addActionListener(this);
+		this.add(refresh);
 		
 		//
 		//openPlayerTablePanel.doClick();
@@ -45,6 +105,21 @@ public class MainFrame extends MyFrame implements ActionListener{
 			loadingPanel.setVisible(false);
 			
 		}
+		
+		if(events.getSource() == exitBt){
+			System.exit(0);
+		}
+		
+		if(events.getSource() == miniBt){
+			this.setExtendedState(JFrame.ICONIFIED);
+		}
+		
+		if(events.getSource() == refresh){
+			mimiButton.setIcon(ImageBin.getImage("mini1"));
+			exitButton.setIcon(ImageBin.getImage("exit1"));
+		}
+		
+		
 	}
 	
 
