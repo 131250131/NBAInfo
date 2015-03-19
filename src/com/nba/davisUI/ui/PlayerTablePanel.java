6df01c0b5457fec75df11003caabd3fbde4375aa
131250131 <275838287@qq.com>
2015-03-19 19:07:02 +0800

    123

--- src/com/nba/davisUI/ui/PlayerTablePanel.java
+++ src/com/nba/davisUI/ui/PlayerTablePanel.java
@@ -32,9 +32,23 @@ public class PlayerTablePanel extends MyPanel{
 		this.setBounds(0, 0, 1280, 720);
 		this.setBackground(Color.WHITE);
 		
-		String[] temp = {"球员姓名","球员得分","球员出手数","球员篮板"};
+		/*
+		 * 球员名称，所属球队，参赛场数，先发场数，篮板数，助攻数，在
+			场时间，投篮命中率，三分命中率，罚球命中率，进攻数，防守数，抢断数，盖
+			帽数，失误数，犯规数， 得分，效率， GmSc 效率值，真实命中率，投篮效率，
+			篮板率，进攻篮板率，防守篮板率，助攻率，抢断率，盖帽率，失误率，使用率
+			传入的数据均要匹配
+		 * */
+		/*
+		 * 
+		 * 
+		 * */
+		String[] temp = {"球员姓名","所属球队","参赛场数","先发场数","篮板数","助攻数","在场时间"
+						,"投篮命中率","三分命中率","罚球命中率","进攻篮板","防守篮板","抢断数","盖帽数",
+						"失误数","犯规数","球员得分","GmSc效率值","真实命中率","投篮效率","篮板率","进攻篮板率",
+						"防守篮板绿","助攻率","抢断绿","盖帽率","失误率","使用率"};
 		temp2 = temp;
-		data2 = new String[4][];
+		data2 = new String[28][];
 		table = new MyTable(temp, data2);
 		
 		JScrollPane jsp=new JScrollPane(table);
@@ -51,7 +65,7 @@ public class PlayerTablePanel extends MyPanel{
 	@SuppressWarnings("unchecked")
 	public void getData(){
 		
-		Object[][] data = new Object[RegisterList.getPlayerNumber()][4];
+		Object[][] data = new Object[RegisterList.getPlayerNumber()][28];
 		
 		System.out.println(RegisterList.getPlayerNumber());
 		
