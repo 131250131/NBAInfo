97e97bc25ec028e1334286340e3581027bf0e4b1
131250208 <504757794@qq.com>
2015-03-19 23:44:26 +0800

    更改了一下mytable

--- src/com/nba/ui/MyTable.java
+++ src/com/nba/ui/MyTable.java
@@ -1,8 +1,10 @@
 package com.nba.ui;
 
+import java.awt.AWTEvent;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Toolkit;
+import java.awt.event.AWTEventListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -16,6 +18,7 @@ import javax.swing.plaf.basic.BasicScrollBarUI;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
 
 public class MyTable extends JScrollPane{
 	
@@ -23,6 +26,16 @@ public class MyTable extends JScrollPane{
 
 	JSortTable table;
 	DefaultTableModel model;
+	String[] columnNames;//表头
+	int upLimit=0;
+	int downLimit;
+	int leftLimit=0;
+	int rightLimit;
+	
+	
+//	TableColumn tableColumn;//用于编辑每一列的颜色
+	DefaultTableCellRenderer render1;//表格渲染器
+//	DefaultTableCellRenderer backGroundColor ;//单元格颜色渲染
 	/*以数据为参数构造表格*/
 	public MyTable(String[] columnNames,Object[][] content){
 		initial(columnNames,content);//初始化
@@ -52,6 +65,9 @@ public class MyTable extends JScrollPane{
 	}
 	
 	void initial(String[] columnNames,Object[][] content){//表格header和二维数组内容，object数组可以装载所有对象，包括基本类型
+		this.columnNames=columnNames;
+		this.downLimit=content.length-1;
+		this.rightLimit=columnNames.length-1;
 		//滚动板的初始化
 		this.setOpaque(false);//设置透明
 		this.getViewport().setOpaque(false);//设置透明
@@ -72,11 +88,13 @@ public class MyTable extends JScrollPane{
 		
 		//表格的初始化 一些美化
 		table.setOpaque(false);
-		table.setSelectionForeground(Color.white);//选中字体颜色
+		table.setSelectionForeground(Color.black);//选中字体颜色
+		table.setSelectionBackground(Color.yellow);
+		table.setBackground(Color.BLACK);
 		table.getTableHeader().setReorderingAllowed(false);//列不可移动
 		
-        DefaultTableCellRenderer render1 = new DefaultTableCellRenderer();   
-        render1.setOpaque(false); //将渲染器设置为透明  
+		render1= new DefaultTableCellRenderer();   
+//        render1.setOpaque(false); //将渲染器设置为透明  
         render1.setHorizontalAlignment(JLabel.CENTER);//居中
         table.setDefaultRenderer(Object.class,render1);  
         
@@ -85,13 +103,18 @@ public class MyTable extends JScrollPane{
 		table.setFont(new Font("Arail", Font.PLAIN, 14));//字体
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);//固定大小
 		
-		
-		
+		//表头
 		JTableHeader head = table.getTableHeader();
 		head.setBackground(new Color(0.1f, 0.19f, 0.54f));
 		head.setForeground(Color.WHITE);
 		head.setResizingAllowed(false);
 		
+ 
+//        //DefaultTableCellRenderer类可以绘制单元格的背景、字体颜色等功能   
+//        backGroundColor= new DefaultTableCellRenderer();   
+//        //绘制部门列的背景为黄色   
+//        backGroundColor.setBackground(Color.yellow);   
+
         //将表格装载到滚动板上
         this.setViewportView(table);
         
@@ -106,25 +129,53 @@ public class MyTable extends JScrollPane{
 		model.setDataVector(content, columnNames);
 		table.updateUI();
 	}
+	/*检查越界并规范*/
+	int avoidOverBoundary(int selectRow){
+		int result=selectRow;
+		if(selectRow<upLimit){
+			selectRow=upLimit;
+		}
+		else if(selectRow<leftLimit){
+			selectRow=leftLimit;
+		}
+		else if(selectRow>downLimit){
+			selectRow=downLimit;
+		}
+		else if(selectRow>rightLimit){
+			selectRow=rightLimit;
+		}
+		return result;
+	}
 	/*选中行向上移动*/
 	public void upMove(){
 		int selectRow=table.getSelectedRow();
-		table.setRowSelectionInterval(selectRow-1,selectRow-1);
+		int nextRow=avoidOverBoundary(selectRow-1);
+//		table.setRowSelectionInterval(nextRow,nextRow);
 	}
 	/*选中行向下移动*/
 	public void downMove(){
 		int selectRow=table.getSelectedRow();
-		table.setRowSelectionInterval(selectRow+1,selectRow+1);
+		int nextRow=avoidOverBoundary(selectRow+1);
+//		table.setRowSelectionInterval(nextRow,nextRow);
 	}
 	/*选中行向左移动*/
 	public void leftMove(){
-		int selectRow=table.getSelectedColumn();
-		table.setColumnSelectionInterval(selectRow-1, selectRow-1);
+		int selectColumn=table.getSelectedColumn();
+		int nextColumn=avoidOverBoundary(selectColumn-1);
+//		table.setColumnSelectionInterval(nextColumn, nextColumn);
+		//TableColumn主要作用可以对表格中的某一列的属性进行编辑   
+//		TableColumn tableColumn = table.getColumn(columnNames[nextColumn]);
+//        tableColumn.setCellRenderer(backGroundColor);  
+		
 	}
 	/*选中行向右移动*/
 	public void rightMove(){
-		int selectRow=table.getSelectedColumn();
-		table.setColumnSelectionInterval(selectRow+1, selectRow+1);
+		int selectColumn=table.getSelectedColumn();
+		int nextColumn=avoidOverBoundary(selectColumn+1);
+//		table.setColumnSelectionInterval(nextColumn, nextColumn);
+		//TableColumn主要作用可以对表格中的某一列的属性进行编辑   
+//		TableColumn tableColumn = table.getColumn(columnNames[nextColumn]);
+//        tableColumn.setCellRenderer(backGroundColor);  
 	}
 	/*滚动条外观重写*/
 	class CustomScrollBarUI extends BasicScrollBarUI{
@@ -166,29 +217,39 @@ public class MyTable extends JScrollPane{
 		testJP.setBackground(Color.black);
 		
 		final MyTable testT=new MyTable();
-		testT.setBounds(0, 0, 60, 200);
+		testT.setBounds(0, 0, 960, 200);
 		
 		
 		testJP.add(testT);
 		testF.add(testJP);
 		
-		//给窗口加键盘监听
-		testF.addKeyListener(new KeyAdapter(){
-	    	public void keyPressed(KeyEvent e){    		
-	    		if(e.getKeyCode()==KeyEvent.VK_UP){
-	    			testT.upMove();
-	    		}
-	    		else if(e.getKeyCode()==KeyEvent.VK_DOWN){
-	    			testT.downMove();
-	    		}
-	    		else if(e.getKeyCode()==KeyEvent.VK_LEFT){
-	    			testT.leftMove();
-	    		}
-	    		else if(e.getKeyCode()==KeyEvent.VK_RIGHT){
-	    			testT.rightMove();
-	    		}
-	      }
-	    });
+		//全局键盘监控
+//		Toolkit.getDefaultToolkit().addAWTEventListener(new AWTEventListener() {
+//		    public void eventDispatched(AWTEvent event) {
+//		        if (((KeyEvent) event).getID() == KeyEvent.KEY_PRESSED) {
+//		            //放入自己的键盘监听事件
+//		        	if(((KeyEvent) event).getKeyCode()==KeyEvent.VK_UP){
+//		    			testT.upMove();
+//		    		}
+//		    		else if(((KeyEvent) event).getKeyCode()==KeyEvent.VK_DOWN){
+//		    			testT.downMove();
+//		    		}
+//		    		else if(((KeyEvent) event).getKeyCode()==KeyEvent.VK_LEFT){
+//		    			testT.leftMove();
+//		    		}
+//		    		else if(((KeyEvent) event).getKeyCode()==KeyEvent.VK_RIGHT){
+//		    			testT.rightMove();
+//		    		}
+//		        }
+//		    }
+//		}, AWTEvent.KEY_EVENT_MASK);
+
+//		//给窗口加键盘监听
+//		testT.addKeyListener(new KeyAdapter(){
+//	    	public void keyPressed(KeyEvent e){    		
+//	    		
+//	      }
+//	    });
 		testF.setVisible(true);
 	}
 }
