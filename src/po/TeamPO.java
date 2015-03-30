0c6929f7212d659f5829a1a59cc0aa3942f64fc9
131250131 <275838287@qq.com>
2015-03-30 15:28:57 +0800

    ss

--- src/po/TeamPO.java
+++ src/po/TeamPO.java
@@ -1,5 +1,145 @@
 package po;
 
 public class TeamPO {
-
+//super key 
+	//球队名称： 
+	String teamName;
+	//球队缩写：
+	String shortName;
+	
+	//球队助攻数（*****需要更新*****）
+		private int teamAssists;
+	//球队投篮命中数（*****需要更新*****）
+		private int teamFG ;
+	//球队投篮出手次数（*****需要更新*****）
+		private int teamFGTry ;
+	//球队三分命中数（*****需要更新*****）
+		private int team3FG ;
+	//球队三分出手次数（*****需要更新*****）
+		private int team3FGTry ;
+	//球队球队罚球命中数（*****需要更新*****）
+		private int teamFTG;
+	//球队罚球出手数（*****需要更新*****）
+		private int teamFTGTry;
+	//球队进攻篮板数（*****需要更新*****）
+		private int teamOffenceRebounds;
+	//球队防守篮板数（*****需要更新*****）
+		private int teamDeffenceRebounds;
+	//球队总篮板数（*****需要更新*****）
+		private int teamTotalRebounds;
+	//球队抢断数（*****需要更新*****）
+		private int teamSteals;
+	//球队盖帽数（*****需要更新*****）
+		private int teamBlocks;
+	//球队失误数（*****需要更新*****）
+		private int teamTurnovers;
+	//球队犯规数（*****需要更新*****）
+		private int teamFouls;
+	//球队总得分数（*****需要更新*****）
+		private int teamScores;
+		
+		public String getTeamName() {
+			return teamName;
+		}
+		public void setTeamName(String teamName) {
+			this.teamName = teamName;
+		}
+		public String getShortName() {
+			return shortName;
+		}
+		public void setShortName(String shortName) {
+			this.shortName = shortName;
+		}
+		public int getTeamAssists() {
+			return teamAssists;
+		}
+		public void setTeamAssists(int teamAssists) {
+			this.teamAssists = teamAssists;
+		}
+		public int getTeamFG() {
+			return teamFG;
+		}
+		public void setTeamFG(int teamFG) {
+			this.teamFG = teamFG;
+		}
+		public int getTeamFGTry() {
+			return teamFGTry;
+		}
+		public void setTeamFGTry(int teamFGTry) {
+			this.teamFGTry = teamFGTry;
+		}
+		public int getTeam3FG() {
+			return team3FG;
+		}
+		public void setTeam3FG(int team3fg) {
+			team3FG = team3fg;
+		}
+		public int getTeam3FGTry() {
+			return team3FGTry;
+		}
+		public void setTeam3FGTry(int team3fgTry) {
+			team3FGTry = team3fgTry;
+		}
+		public int getTeamFTG() {
+			return teamFTG;
+		}
+		public void setTeamFTG(int teamFTG) {
+			this.teamFTG = teamFTG;
+		}
+		public int getTeamFTGTry() {
+			return teamFTGTry;
+		}
+		public void setTeamFTGTry(int teamFTGTry) {
+			this.teamFTGTry = teamFTGTry;
+		}
+		public int getTeamOffenceRebounds() {
+			return teamOffenceRebounds;
+		}
+		public void setTeamOffenceRebounds(int teamOffenceRebounds) {
+			this.teamOffenceRebounds = teamOffenceRebounds;
+		}
+		public int getTeamDeffenceRebounds() {
+			return teamDeffenceRebounds;
+		}
+		public void setTeamDeffenceRebounds(int teamDeffenceRebounds) {
+			this.teamDeffenceRebounds = teamDeffenceRebounds;
+		}
+		public int getTeamTotalRebounds() {
+			return teamTotalRebounds;
+		}
+		public void setTeamTotalRebounds(int teamTotalRebounds) {
+			this.teamTotalRebounds = teamTotalRebounds;
+		}
+		public int getTeamSteals() {
+			return teamSteals;
+		}
+		public void setTeamSteals(int teamSteals) {
+			this.teamSteals = teamSteals;
+		}
+		public int getTeamBlocks() {
+			return teamBlocks;
+		}
+		public void setTeamBlocks(int teamBlocks) {
+			this.teamBlocks = teamBlocks;
+		}
+		public int getTeamTurnovers() {
+			return teamTurnovers;
+		}
+		public void setTeamTurnovers(int teamTurnovers) {
+			this.teamTurnovers = teamTurnovers;
+		}
+		public int getTeamFouls() {
+			return teamFouls;
+		}
+		public void setTeamFouls(int teamFouls) {
+			this.teamFouls = teamFouls;
+		}
+		public int getTeamScores() {
+			return teamScores;
+		}
+		public void setTeamScores(int teamScores) {
+			this.teamScores = teamScores;
+		}
+		
+	
 }
