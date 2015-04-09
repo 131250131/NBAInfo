7b0fe4fce26fbecc4c8e9df1fca8663f8202d185
131250131 <275838287@qq.com>
2015-04-09 09:56:14 +0800

    123

---
+++ src/logic/MatchController.java
@@ -0,0 +1,24 @@
+package logic;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import vo.MatchVO;
+
+public class MatchController {
+		ArrayList<Match> allMatches = new ArrayList<Match>();
+		ArrayList<Match> matchesOfTaday = new ArrayList<Match>();
+		
+		public ArrayList<MatchVO> getAllMatchVO(){
+			return null;
+		}
+		
+		public ArrayList<MatchVO> getMacthVOOfToday(Date date){
+			return null;
+		}
+		
+		
+		
+		
+		
+}
