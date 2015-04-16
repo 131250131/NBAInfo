956ab93303386da3ca96cd768257709f995828f3
131250131 <275838287@qq.com>
2015-04-16 23:15:00 +0800

    都是周尧写的。

---
+++ src/Utibility/JudgeUpdate.java
@@ -0,0 +1,40 @@
+package Utibility;
+
+import java.io.File;
+
+public class JudgeUpdate implements Runnable {
+	int number;
+	String filePath;
+	private boolean Hasupdate=false;
+     public JudgeUpdate(String filePath) {
+		// TODO Auto-generated constructor stub
+ 		File file = new File(filePath);                
+ 		File[] File = file.listFiles();
+ 		this.filePath=filePath;
+ 		number=File.length;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		File file = new File(filePath);                
+ 		File[] File = file.listFiles();
+		for(;;){
+		   if(File.length!=number){
+			   boolean Hasupdate=true;
+			   number=File.length;
+		   }
+		   try{
+			   Thread.sleep(10000);
+		   }catch(Exception e){
+			   System.out.println("获取数据超时");
+		   }
+		}
+		
+	}
+   public void initHasupdate(){
+	   Hasupdate =false;
+   }
+   public boolean getHasupdate(){
+	   return Hasupdate;
+   }
+}
--- src/vo/MatchVO.java
+++ src/vo/MatchVO.java
@@ -1,5 +1,80 @@
-package vo;
+﻿package vo;
+
+import java.util.ArrayList;
+
+
 
 public class MatchVO {
-
+	int Num;//比赛场次的编号，从零开始，
+	String season;
+	String date;
+	TeamVO leftTeam;
+	TeamVO rightTeam;
+	String score;
+	ArrayList<PlayerVO> playerlist=new ArrayList<PlayerVO>();
+//每一节的具体得分；
+	String scores1;
+	String scores2;
+	String scores3;
+	String scores4;
+	public int getNum() {
+		return Num;
+	}
+	public void setNum(int num) {
+		Num = num;
+	}
+	public String getSeason() {
+		return season;
+	}
+	public void setSeason(String season) {
+		this.season = season;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public TeamVO getLeftTeam() {
+		return leftTeam;
+	}
+	public void setLeftTeam(TeamVO leftTeam) {
+		this.leftTeam = leftTeam;
+	}
+	public TeamVO getRightTeam() {
+		return rightTeam;
+	}
+	public void setRightTeam(TeamVO rightTeam) {
+		this.rightTeam = rightTeam;
+	}
+	public String getScores1() {
+		return scores1;
+	}
+	public void setScores1(String scores1) {
+		this.scores1 = scores1;
+	}
+	public String getScores2() {
+		return scores2;
+	}
+	public void setScores2(String scores2) {
+		this.scores2 = scores2;
+	}
+	public String getScores3() {
+		return scores3;
+	}
+	public void setScores3(String scores3) {
+		this.scores3 = scores3;
+	}
+	public String getScores4() {
+		return scores4;
+	}
+	public void setScores4(String scores4) {
+		this.scores4 = scores4;
+	}
+	public void addplayer(PlayerVO player){
+		playerlist.add(player);
+	}
+	public void setScore(String score){
+		this.score=score;
+	}
 }
