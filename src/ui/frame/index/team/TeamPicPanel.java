f5f9abead31c4df10d22553685d7869286133424
david941211 <657834200@qq.com>
2015-05-03 14:02:36 +0800

    qwe

--- src/ui/frame/index/team/TeamPicPanel.java
+++ src/ui/frame/index/team/TeamPicPanel.java
@@ -10,6 +10,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import ui.myUI.ImageLabel;
+import ui.system.ChineseTranslator;
+import ui.system.Controller;
 import ui.system.ImageSaver;
 import ui.system.UIData;
 
@@ -17,8 +19,12 @@ import ui.system.UIData;
 public class TeamPicPanel extends JPanel implements ActionListener{
 
 	JButton button;
+	String _teamName;
 	
 	public TeamPicPanel(int x, int y , int width, int height, String teamName){
+		
+		_teamName = teamName;
+		
 		this.setVisible(true);
 		this.setBounds(x, y, width, height);
 		this.setLayout(null);
@@ -32,7 +38,7 @@ public class TeamPicPanel extends JPanel implements ActionListener{
 		int textHeight = (int) (30 * UIData.changeY);
 		int blank = (int) (0 * UIData.changeY);
 		
-		JLabel teamNameLable = new JLabel(getTeamChineseName(teamName), JLabel.CENTER);
+		JLabel teamNameLable = new JLabel(ChineseTranslator.TeamNameTrans(teamName), JLabel.CENTER);
 		teamNameLable.setBounds(0, height - textHeight - blank, width, textHeight);
 		teamNameLable.setVisible(true);
 		teamNameLable.setForeground(Color.WHITE);
@@ -57,109 +63,11 @@ public class TeamPicPanel extends JPanel implements ActionListener{
 		this.add(button);
 	}
 	
-	private String getTeamChineseName(String teamName){
-		String name = null;
-		switch(teamName){
-		case "DAL":	
-			name = "小牛";
-			break;
-		case "DEN":	
-			name = "掘金";
-			break;
-		case "GSW":	
-			name = "勇士";
-			break;	
-		case "HOU":	
-			name = "火箭";
-			break;	
-		case "LAC":	
-			name = "快船";
-			break;	
-		case "LAL":	
-			name = "湖人";
-			break;	
-		case "MEM":	
-			name = "灰熊";
-			break;	
-		case "MIN":	
-			name = "灰狼";
-			break;
-		case "NOP":	
-			name = "鹈鹕";
-			break;
-		case "POR":	
-			name = "拓荒者";
-			break;
-		case "OKC":	
-			name = "雷霆";
-			break;
-		case "PHX":	
-			name = "太阳";
-			break;
-		case "SAC":	
-			name = "国王";
-			break;
-		case "SAS":	
-			name = "马刺";
-			break;
-		case "UTA":	
-			name = "爵士";
-			break;
-		case "ATL":
-			name = "老鹰";
-			break;
-		case "BKN":	
-			name = "篮网";
-			break;
-		case "BOS":	
-			name = "凯尔特人";
-			break;
-		case "CHA":	
-			name = "黄蜂";
-			break;
-		case "CHI":	
-			name = "公牛";
-			break;
-		case "CLE":	
-			name = "骑士";
-			break;
-		case "DET":	
-			name = "活塞";
-			break;
-		case "IND":	
-			name = "步行者";
-			break;
-		case "MIA":	
-			name = "热火";
-			break;
-		case "MIL":	
-			name = "雄鹿";
-			break;
-		case "NYK":	
-			name = "尼克斯";
-			break;
-		case "ORL":	
-			name = "魔术";
-			break;
-		case "PHI":	
-			name = "76人";
-			break;
-		case "TOR":	
-			name = "猛龙";
-			break;
-		case "WAS":	
-			name = "奇才";
-			break;
-		}	
-	return name;
-
-	}
-	
 	@Override
 	public void actionPerformed(ActionEvent events) {
 		
 		if(events.getSource() == button){
-			
+			Controller.addTeamPanel(_teamName);
 		}
 	}
 
---
+++ src/ui/frame/team/TeamPanel.java
@@ -0,0 +1,46 @@
+package ui.frame.team;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import ui.system.UIData;
+import vo.TeamVO;
+
+@SuppressWarnings("serial")
+public class TeamPanel extends JPanel{
+	
+	int width = (int) (1720 * UIData.changeX);
+	int height = (int) (3000 * UIData.changeY);
+	
+	public TeamPanel(TeamVO team){
+		this.setLayout(null);
+		this.setPreferredSize(new Dimension(width, height));
+		this.setOpaque(false);
+		this.setVisible(true);
+		
+		JLabel title = new JLabel("球员信息");
+		title.setBounds((int) (285 * UIData.changeX), (int) (30 * UIData.changeY), (int) (200 * UIData.changeX), (int) (30 * UIData.changeY));
+		title.setForeground(Color.LIGHT_GRAY);
+		title.setFont(new Font("新細明體", Font.BOLD, (int)(30 * UIData.changeY)));
+		title.setVisible(true);
+		this.add(title);
+		
+		int labelX = (int) (200 * UIData.changeX);
+		int labelWidth = (int) (1410 * UIData.changeX);
+		int playerLabelY = (int) (100 * UIData.changeY);
+		int playerLabelHeight = (int) (300 * UIData.changeY);
+		TeamInfoLabel teamInfoLabel = new TeamInfoLabel(labelX, playerLabelY, labelWidth, playerLabelHeight, team);
+		this.add(teamInfoLabel);
+		
+		JPanel backPanel = new JPanel();
+		backPanel.setBounds(labelX, 0, labelWidth, height);
+		backPanel.setBackground(new Color(0, 0, 0, 0.8f));
+		backPanel.setVisible(true);
+		this.add(backPanel);
+	}
+
+}
