dee8c70f91eca124d8b63219b6400413cfc3e3a6
131250105 <465033064@qq.com>
2015-05-10 09:57:54 +0800

    .

--- src/vo/PlayerVO.java
+++ src/vo/PlayerVO.java
@@ -1200,8 +1200,9 @@ public class PlayerVO implements Comparable<PlayerVO>{
 				return result;
 			}
 		   
-	       public PlayerHighInfo getPlayerHighInfo(){
+	       public PlayerHighInfo getPlayerHighInfo(String option){
 	    	   PlayerHighInfo playerhighInfo =new PlayerHighInfo();
+	    	   if(option.equals("aveg")){
 	    	   playerhighInfo.setAssistEfficient(AR);
 	    	   playerhighInfo.setBlockShotEfficient(BR);
 	    	   playerhighInfo.setDefendReboundEfficient(d_RR);
@@ -1211,15 +1212,33 @@ public class PlayerVO implements Comparable<PlayerVO>{
 	    	   playerhighInfo.setName(playerName);
 	    	   playerhighInfo.setOffendReboundEfficient(o_RR);
 	    	   playerhighInfo.setPosition(position);
-	    	   //playerhighInfo.setRealShot(realShot);
+	    	   playerhighInfo.setRealShot(playerTSP);
 	    	   playerhighInfo.setReboundEfficient(RR);
-	    	   //playerhighInfo.setShotEfficient();
+	    	   playerhighInfo.setShotEfficient(playerSER);
 	    	   playerhighInfo.setStealEfficient(SR);
 	    	   playerhighInfo.setTeamName(teamShortName);
+	    	   }
+	    	   else{
+	    		   playerhighInfo.setAssistEfficient(AR);
+		    	   playerhighInfo.setBlockShotEfficient(BR);
+		    	   playerhighInfo.setDefendReboundEfficient(d_RR);
+		    	   playerhighInfo.setFaultEfficient(TR);
+		    	   playerhighInfo.setGmSc(playerGmScER);
+		    	   playerhighInfo.setLeague("east");
+		    	   playerhighInfo.setName(playerName);
+		    	   playerhighInfo.setOffendReboundEfficient(o_RR);
+		    	   playerhighInfo.setPosition(position);
+		    	   playerhighInfo.setRealShot(playerTSP);
+		    	   playerhighInfo.setReboundEfficient(RR);
+		    	   playerhighInfo.setShotEfficient(playerSER);
+		    	   playerhighInfo.setStealEfficient(SR);
+		    	   playerhighInfo.setTeamName(teamShortName);
+	    	   }
 	    	   return playerhighInfo;
 	       }
-		  public PlayerNormalInfo getPlayerNormalInfo(){
+		  public PlayerNormalInfo getPlayerNormalInfo(String option){
 			  PlayerNormalInfo pl=new PlayerNormalInfo();
+			  if(option.equals("aveg")){
 			  pl.setAge(playerAge);
 			  pl.setAssist(aver_playerAssists);
 			  pl.setBlockShot(aver_playerBlocks);
@@ -1239,6 +1258,28 @@ public class PlayerVO implements Comparable<PlayerVO>{
 			  pl.setSteal(aver_playerSteals);
 			  pl.setTeamName(teamShortName);
 			  pl.setThree(player3FGP);
+			  }
+			  else{
+				  pl.setAge(playerAge);
+				  pl.setAssist(playerAssists);
+				  pl.setBlockShot(playerBlocks);
+				  pl.setDefend(playerDeffenceRebounds);
+				  pl.setEfficiency(playerPER);
+				  pl.setFault(playerTurnovers);
+				  pl.setFoul(playerFouls);
+				  pl.setMinute(playerPlayTime);
+				  pl.setName(playerName);
+				  pl.setNumOfGame(Integer.parseInt(playerNumber));
+				  pl.setOffend(playerOffenceRebounds);
+				  pl.setPenalty(playerFTGP);
+				  pl.setPoint(playerScores);
+				  pl.setRebound(playerTotalRebounds);
+				  pl.setShot(playerFGP);
+				  pl.setStart(startTimes);
+				  pl.setSteal(playerSteals);
+				  pl.setTeamName(teamShortName);
+				  pl.setThree(player3FGP);
+			  }
 			  return pl;
 		  }
 		  public PlayerHotInfo getPLayerHOTInfo(String f){
@@ -1248,7 +1289,7 @@ public class PlayerVO implements Comparable<PlayerVO>{
 			  pl.setPosition(position);
 			  pl.setTeamName(teamShortName);
 			  pl.setUpgradeRate(advancedP);//!
-			  pl.setValue();
+			  pl.setValue(this.getdatebystring(f, "aveg"));
 			  return pl;
 		  }
           public PlayerKingInfo getPlayerKingInfo(String f){
@@ -1257,10 +1298,9 @@ public class PlayerVO implements Comparable<PlayerVO>{
         	  pl.setName(playerName);
         	  pl.setPosition(position);
         	  pl.setTeamName(teamShortName);
-        	  pl.setValue();
+        	  pl.setValue(this.getdatebystring(f, "aveg"));
         	  return pl;
-//<<<<<<< HEAD
-//=======
+
           }
           
           public double getdatebystring(String f,String option){
