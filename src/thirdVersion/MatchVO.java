e7082fd1428d3e132584f3e352adb77e1c947c1d
131250131 <275838287@qq.com>
2015-06-03 01:06:08 +0800

    数学计算器类是用来算各种统计方面的数值的;还没写好。

---
+++ src/thirdVersion/MatchVO.java
@@ -0,0 +1,96 @@
+package thirdVersion;
+
+public class MatchVO {
+	String matchID;
+	String date;//比赛日期.time
+	String leftTeamName;
+	String rightTeamName;
+	String firstScore;
+	String secondScore;
+	String thirdScore;
+	String forthScore;
+	String exttaScore;
+	String totalScore;
+	
+	public String getMatchID() {
+		return matchID;
+	}
+	
+	public void setMatchID(String matchID) {
+		this.matchID = matchID;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getLeftTeamName() {
+		return leftTeamName;
+	}
+	
+	public void setLeftTeamName(String leftTeamName) {
+		this.leftTeamName = leftTeamName;
+	}
+	
+	public String getRightTeamName() {
+		return rightTeamName;
+	}
+	
+	public void setRightTeamName(String rightTeamName) {
+		this.rightTeamName = rightTeamName;
+	}
+	
+	public String getFirstScore() {
+		return firstScore;
+	}
+	
+	public void setFirstScore(String firstScore) {
+		this.firstScore = firstScore;
+	}
+	
+	public String getSecondScore() {
+		return secondScore;
+	}
+	
+	public void setSecondScore(String secondScore) {
+		this.secondScore = secondScore;
+	}
+	
+	public String getThirdScore() {
+		return thirdScore;
+	}
+	
+	public void setThirdScore(String thirdScore) {
+		this.thirdScore = thirdScore;
+	}
+	
+	public String getForthScore() {
+		return forthScore;
+	}
+	
+	public void setForthScore(String forthScore) {
+		this.forthScore = forthScore;
+	}
+	
+	public String getExttaScore() {
+		return exttaScore;
+	}
+	public void setExttaScore(String exttaScore) {
+		this.exttaScore = exttaScore;
+	}
+	
+	public String getTotalScore() {
+		return totalScore;
+	}
+	
+	public void setTotalScore(String totalScore) {
+		this.totalScore = totalScore;
+	}
+	
+	
+	
+}
---
+++ src/thirdVersion/MathCalculator.java
@@ -0,0 +1,17 @@
+package thirdVersion;
+
+import java.util.ArrayList;
+
+//这个类用来计算各种数据;
+public class MathCalculator {
+	//计算平均数
+	public double getAverage(ArrayList<Double> tempList){
+		return 0;
+	}
+	
+	/*
+	 * 方差
+	 * 标准差
+	 * 
+	 * */
+}
