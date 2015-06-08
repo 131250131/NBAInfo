7bfea2f536289e7a6a8bbf4a70933cecf7fabca3
131250105 <465033064@qq.com>
2015-06-08 18:55:59 +0800

    .

--- src/thirdVersion/MathCalculator.java
+++ src/thirdVersion/MathCalculator.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 
 //这个类用来计算各种数据;
 public class MathCalculator {
-	//计算平均数
+	//计算样本的平均数，可以作为总体期望的无偏估计
 	public double getAverage(ArrayList<Double> tempList){
-		return 0;
+		   int n=tempList.size();
+		   double total=0;
+		   for(Double d:tempList){
+			   total+=d;
+		   }
+		   return total/n;
 	}
-	
-	
 	
 	/*
 	 * 方差
