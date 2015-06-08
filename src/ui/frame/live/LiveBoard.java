f11c336eb38ad6bd3c391ba4e7df44993e2ec2f6
david941211 <657834200@qq.com>
2015-06-08 20:40:45 +0800

    dd

--- src/ui/frame/live/LiveBoard.java
+++ src/ui/frame/live/LiveBoard.java
@@ -1,22 +1,27 @@
 package ui.frame.live;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.Timer;
 
 import ui.system.UIData;
 
 @SuppressWarnings("serial")
-public class LiveBoard extends JScrollPane{
+public class LiveBoard extends JScrollPane implements ActionListener{
 	
-	int width = (int) (1100 * UIData.changeX);
+	int width = (int) (1200 * UIData.changeX);
 	int height = (int) (600 * UIData.changeY);
-	int x = (int) ((1920 - width)/2 * UIData.changeX);
-	int y = (int) (200 * UIData.changeY);
+	int x = (int) ((1920 - width)/2 * UIData.changeX - 50);
+	int y = (int) ((200 - 40 + 200 + 50) * UIData.changeY);
 	
 	static int MaxSize = 700;
 	
@@ -34,12 +39,32 @@ public class LiveBoard extends JScrollPane{
 		this.getVerticalScrollBar().setOpaque(false);
 		this.getVerticalScrollBar().setBorder(BorderFactory.createEmptyBorder());
 		this.getVerticalScrollBar().setUnitIncrement(35);
-	//	this.setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_NEVER);
+		this.setHorizontalScrollBarPolicy(HORIZONTAL_SCROLLBAR_NEVER);
 
 		getMaxSize();
 		
 		largeLiveBoard = new LargeLiveBoard();
 		this.setViewportView(largeLiveBoard);
+		
+		turnTo1 = new JButton();
+		turnTo1.addActionListener(this);
+		this.add(turnTo1);
+		
+		turnTo2 = new JButton();
+		turnTo2.addActionListener(this);
+		this.add(turnTo2);
+		
+		turnTo3 = new JButton();
+		turnTo3.addActionListener(this);
+		this.add(turnTo3);
+		
+		turnTo4 = new JButton();
+		turnTo4.addActionListener(this);
+		this.add(turnTo4);
+		
+		turnToOT = new JButton();
+		turnToOT.addActionListener(this);
+		this.add(turnToOT);
 	}
 	
 	private void getMaxSize(){
@@ -80,6 +105,59 @@ public class LiveBoard extends JScrollPane{
 			wordsArray[4].add("5asd " + (i + 1));
 		}
 	}
+	
+	public static JButton turnTo1, turnTo2, turnTo3, turnTo4, turnToOT;
+
+	int currentLoction;
+	int destination;
+	int step;
+	int move = 0;
+	int time = 3;
+	Timer timer = new Timer(1, this);
+	
+	private void setScrollLoction(int _destination){
+		currentLoction = this.getHorizontalScrollBar().getValue();
+		destination = _destination;
+		move = 0;
+		int height = 400;
+		time = Math.abs((destination - currentLoction)) / height + 3;
+		
+		step = (destination - currentLoction) / time;
+		timer.start();	
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == timer){
+			if(move == time){
+				timer.stop();				
+			}else{
+				move++;
+				int value = currentLoction + step * move;
+				this.getHorizontalScrollBar().setValue(value);
+			}			
+		}
+		
+		if(e.getSource() == turnTo1){
+			setScrollLoction(0);
+		}
+		
+		if(e.getSource() == turnTo2){
+			setScrollLoction(1 * 1200);
+		}
+		
+		if(e.getSource() == turnTo3){
+			setScrollLoction(2 * 1200);
+		}
+		
+		if(e.getSource() == turnTo4){
+			setScrollLoction(3 * 1200);
+		}
+		
+		if(e.getSource() == turnToOT){
+			setScrollLoction(4 * 1200);
+		}
+	}
 
 }
 
@@ -88,7 +166,7 @@ class LargeLiveBoard extends JPanel{
 	
 	LiveBoardPanel[] liveArray = new LiveBoardPanel[5];
 	
-	int width = (int) (1100 * 5 * UIData.changeX);
+	int width = (int) (1200 * 5 * UIData.changeX);
 	int height = (int) (LiveBoard.MaxSize * UIData.changeY);
 	
 	public LargeLiveBoard(){
@@ -97,10 +175,11 @@ class LargeLiveBoard extends JPanel{
 		this.setPreferredSize(new Dimension(width, height));
 		this.setOpaque(false);
 		this.setVisible(true);
+		this.setBackground(new Color(0,0,0,0));
 		
 		for(int i = 0; i < 5; i++){
 			liveArray[i] = new LiveBoardPanel(i);
-			liveArray[i].setBounds(i * 1100, 0, 1100, height);
+			liveArray[i].setBounds(i * 1200, 0, 1200, height);
 			liveArray[i].setVisible(true);
 			this.add(liveArray[i]);
 		}
@@ -112,6 +191,7 @@ class LiveBoardPanel extends JPanel{
 	
 	public LiveBoardPanel(int i){
 		this.setLayout(null);
+		this.setBackground(null);
 		
 		initWords(LiveBoard.wordsArray[i].size(), i);
 	}
@@ -130,12 +210,18 @@ class WordsLabel extends JPanel{
 	
 	public WordsLabel(String str, int index){
 		int height = 30;
-		this.setBounds(0, height * index, 1100, height);
+		this.setBounds(0, height * index, 1200, height);
 		this.setLayout(null);
+		if(index % 2 == 0){
+			this.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.3f));
+		}else{
+			this.setBackground(new Color(0.4f, 0.4f, 0.4f, 0.25f));
+		}
 
 		JLabel label = new JLabel(str, JLabel.CENTER);
-		label.setBounds(0, 0, 1100, height);
+		label.setBounds(0, 0, 1200, height);
 		label.setVisible(true);
+		label.setForeground(Color.WHITE);
 		this.add(label);
 	}
 	
--- src/ui/frame/live/LivePanel.java
+++ src/ui/frame/live/LivePanel.java
@@ -3,10 +3,15 @@ package ui.frame.live;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import ui.myUI.ImageLabel;
+import ui.system.ImageSaver;
 import ui.system.UIData;
 
 @SuppressWarnings("serial")
@@ -28,9 +33,83 @@ public class LivePanel extends JPanel{
 		title.setVisible(true);
 		this.add(title);
 		
+		int Lwidth = (int) (1200 * UIData.changeX);
+		int Lheight = (int) (600 * UIData.changeY);
+		int Lx = (int) ((1920 - Lwidth)/2 * UIData.changeX - 50);
+		int Ly = (int) ((200 - 40 + 200 + 50) * UIData.changeY);
+		
+		JLabel num1 = new JLabel("109");
+		num1.setHorizontalAlignment(JLabel.CENTER);
+		num1.setFont(new Font("Arail", Font.PLAIN, 40));
+		num1.setBounds(620 - 40, 210, 70, 40);
+		num1.setBackground(null);
+		num1.setForeground(Color.BLACK);
+		num1.setVisible(true);
+		this.add(num1);
+		
+		JLabel num2 = new JLabel("90");
+		num2.setHorizontalAlignment(JLabel.CENTER);
+		num2.setFont(new Font("Arail", Font.PLAIN, 40));
+		num2.setBounds(1225 - 40, 210, 70, 40);
+		num2.setBackground(null);
+		num2.setForeground(Color.BLACK);
+		num2.setVisible(true);
+		this.add(num2);
+		
+		ImageLabel label2 = new ImageLabel(ImageSaver.getIcon("live2"), Lx, Lx - 180, 1200, 187);
+		this.add(label2);
+		
+		JPanel color2 = new JPanel();
+		color2.setBounds(Lx + Lwidth - 20, Ly, 20, Lheight);
+		color2.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.5f));
+		color2.setVisible(true);
+		this.add(color2);
+
 		LiveBoard liveBoard = new LiveBoard();
 		this.add(liveBoard);
 		
+		JPanel color1 = new JPanel();
+		color1.setBounds(Lx, Ly, Lwidth, Lheight);
+		color1.setBackground(new Color(0.1f, 0.1f, 0.1f, 0.5f));
+		color1.setVisible(true);
+		this.add(color1);
+		
+		Buttons[] buttons = new Buttons[6];
+		for(int i = 0; i < 6; i++){
+			buttons[i] = new Buttons(getWords(i), i);
+			this.add(buttons[i]);
+		}
+		
+		buttons[0].addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				LiveBoard.turnTo1.doClick();
+			}
+		});
+		
+		buttons[1].addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				LiveBoard.turnTo2.doClick();
+			}
+		});
+		
+		buttons[2].addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				LiveBoard.turnTo3.doClick();
+			}
+		});
+		
+		buttons[3].addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				LiveBoard.turnTo4.doClick();
+			}
+		});
+		
+		buttons[4].addMouseListener(new MouseAdapter(){
+			public void mouseClicked(MouseEvent e){
+				LiveBoard.turnToOT.doClick();
+			}
+		});
+				
 		int labelX = (int) (200 * UIData.changeX);
 		int labelWidth = (int) (1410 * UIData.changeX);
 		
@@ -40,5 +119,34 @@ public class LivePanel extends JPanel{
 		backPanel.setVisible(true);
 		this.add(backPanel);
 	}
+	
+	private String getWords(int i){
+		switch(i){
+		case 0:
+			return "第一节";
+		case 1:
+			return "第二节";
+		case 2:
+			return "第三节";
+		case 3:
+			return "第四节";
+		case 4:
+			return "加时赛";
+		default:
+			return "详细数据";
+		}
+	}
 
+}
+
+@SuppressWarnings("serial")
+class Buttons extends JButton{
+	
+	public Buttons(String str, int index){
+		this.setText(str);
+		this.setVisible(true);
+		this.setBounds(200 - 72 + 200 * index + 180, 130 + 250, 200, 30);
+		this.setForeground(Color.WHITE);
+		this.setBackground(Color.BLACK);
+	}
 }
