8733fd2310371061e2f18aa7693964b3569c7a48
131250131 <275838287@qq.com>
2015-06-13 15:03:37 +0800

    筛选完成，通过测试。

--- src/thirdVersion/PlayerControllerThird.java
+++ src/thirdVersion/PlayerControllerThird.java
@@ -1115,10 +1115,14 @@ public class PlayerControllerThird implements playerControllerThirdService{
        public static void main(String args[]){
     	   PlayerControllerThird p=new PlayerControllerThird();
     	  // ArrayList<PlayerdatainfoVO> pr=p.getplayerbyteam("金州勇士", "13-14");
-    	   PlayerVO result=new PlayerVO();
-    	   result=p.getPlayervobyname("Larry Bird", "91-92");
-    	   System.out.println(result.getPlayerName()+result.getTeamname()+result.getAver_playerScores());
-   	   
+//    	   PlayerVO result=new PlayerVO();
+//    	   result=p.getPlayervobyname("Larry Bird", "91-92");
+//    	   System.out.println(result.getPlayerName()+result.getTeamname()+result.getAver_playerScores());
+    	   ArrayList<PlayerVO> list = p.getSelectedPlayers("中锋", "14-15", "平均篮板");
+    	   System.out.println(list.size());
+    	   for(PlayerVO vo : list){
+    		   System.out.println(vo.getPlayerName()+" "+vo.getPosition()+" "+vo.getAver_playerTotalRebounds());
+    	   }
 //    	   for(PlayerdatainfoVO l:pr){
 //    		   System.out.println(l.getName());
 //    	   }
@@ -1302,6 +1306,159 @@ public class PlayerControllerThird implements playerControllerThirdService{
 	}catch(Exception e){
 		e.printStackTrace();
 	}
+		return result;
+	}
+	
+	public ArrayList<PlayerVO> getSelectedPlayers(String position,String season,String infoType){
+		ArrayList<PlayerVO> result = new ArrayList<PlayerVO>();
+		ArrayList<PlayerdatainfoVO> ps=new ArrayList<PlayerdatainfoVO>();
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+    		Connection conn =DriverManager.getConnection(url, user, password);
+    		String dataType = "";
+    		switch(infoType){
+    			case "助攻": dataType = "TAssists";break;
+    			case "分钟": dataType = "TTime";break;
+    			case "篮板": dataType = "TRebounds";break;
+    			case "抢断": dataType = "TSteals";break;
+    			case "盖帽": dataType = "TBlocks";break;
+    			case "失误": dataType = "TTurnovers";break;
+    			case "犯规": dataType = "TFouls";break;
+    			case "得分": dataType = "TScores";break;
+    			case "投篮": dataType = "TFGZ";break;
+    			case "三分": dataType = "TSFGZ";break;
+    			case "罚球": dataType = "TFTGZ";break;
+    			case "效率": dataType = "PER";break;
+    			case "平均篮板": dataType = "ARebounds";break;
+    			case "平均助攻": dataType = "AAssists";break;
+    			case "平均抢断": dataType = "ASteals";break;
+    			case "平均得分": dataType = "AScores";break;
+    			case "平均盖帽": dataType = "ABlocks";break;
+    			case "FGP": dataType = "FGP";break;
+    			case "3FGP": dataType = "SFGP";break;
+    			case "FTGP": dataType = "FTGP";break;
+    		}
+    		
+    		sql="SELECT * FROM playerdatainfo WHERE position= '"+position+"'"+"AND season='"+season+"' and isplayoff =0 ORDER BY "+dataType+" DESC;";
+
+    		PreparedStatement pstmt=conn.prepareStatement(sql);
+    		ResultSet rs=pstmt.executeQuery();
+    		int i=0;
+    		System.out.println("dd");
+    		while(rs.next()&&i<=50){
+    			i++;
+    			PlayerdatainfoVO p=new PlayerdatainfoVO();
+    			p.id=rs.getString(1);//球员编号 
+			    p.season=rs.getString(2);//赛季
+			    p.teamname=rs.getString(3);//teamname为总计时，表示该球员一个赛季在两个球队打过
+			    p.attendmatches=Double.parseDouble(df.format(rs.getDouble(4)));
+			    p.First=Double.parseDouble(df.format(rs.getDouble(5)));
+			    p.Atime=Double.parseDouble(df.format(rs.getDouble(6)));
+			    p.FGP=Double.parseDouble(df.format(rs.getDouble(7)*100));
+			    p.AFGZ=Double.parseDouble(df.format(rs.getDouble(8)));//Z代表命中
+			    p.AFG=Double.parseDouble(df.format(rs.getDouble(9)));
+			    p.SFGP=Double.parseDouble(df.format(rs.getDouble(10)*100));
+			    p.ASFGZ=Double.parseDouble(df.format(rs.getDouble(11)));
+			    p.ASFG=Double.parseDouble(df.format(rs.getDouble(12)));
+			    p.FTGP=Double.parseDouble(df.format(rs.getDouble(13)*100));
+			    p.AFTGZ=Double.parseDouble(df.format(rs.getDouble(14)));
+			    p.AFTG=Double.parseDouble(df.format(rs.getDouble(15)));
+			    p.ARebounds=Double.parseDouble(df.format(rs.getDouble(16)));
+			    p.AORebouns=Double.parseDouble(df.format(rs.getDouble(17)));
+			    p.ADRebounds=Double.parseDouble(df.format(rs.getDouble(18)));
+			    p.AAssists=Double.parseDouble(df.format(rs.getDouble(19)));
+			    p.ASteals=Double.parseDouble(df.format(rs.getDouble(20)));
+			    p.ABlocks=Double.parseDouble(df.format(rs.getDouble(21)));
+			    p.ATurnovers=Double.parseDouble(df.format(rs.getDouble(22)));
+			    p.AFeals=Double.parseDouble(df.format(rs.getDouble(23)));
+			    p.AScores=Double.parseDouble(df.format(rs.getDouble(24)));
+			    p.Win=Double.parseDouble(df.format(rs.getDouble(25)));
+			    p.Lose=Double.parseDouble(df.format(rs.getDouble(26)));
+			    p.Ttime=Double.parseDouble(df.format(rs.getDouble(27)));
+			    p.TFGZ=Double.parseDouble(df.format(rs.getDouble(28)));
+			    p.TFG=Double.parseDouble(df.format(rs.getDouble(29)));
+			    p.TSFGZ=Double.parseDouble(df.format(rs.getDouble(30)));
+			    p.TSFG=Double.parseDouble(df.format(rs.getDouble(31)));
+			    p.TFTGZ=Double.parseDouble(df.format(rs.getDouble(32)));
+			    p.TFTG=Double.parseDouble(df.format(rs.getDouble(33)));
+			    p.TRebounds=Double.parseDouble(df.format(rs.getDouble(34)));
+			    p.TORebouns=Double.parseDouble(df.format(rs.getDouble(35)));
+			    p.TDRebounds=Double.parseDouble(df.format(rs.getDouble(36)));
+			    p.TAssists=Double.parseDouble(df.format(rs.getDouble(37)));
+			    p.TSteals=Double.parseDouble(df.format(rs.getDouble(38)));
+			    p.TBlocks=Double.parseDouble(df.format(rs.getDouble(39)));
+			    p.TTurnovers=Double.parseDouble(df.format(rs.getDouble(40)));
+			    p.TFeals=Double.parseDouble(df.format(rs.getDouble(41)));
+			    p.TScores=Double.parseDouble(df.format(rs.getDouble(42)));
+			    p.RR=Double.parseDouble(df.format(rs.getDouble(43)));//篮板率
+			    p.O_RR=Double.parseDouble(df.format(rs.getDouble(44)));//进攻篮板率
+			    p.D_RR=Double.parseDouble(df.format(rs.getDouble(45)));//防守篮板率
+			    p.AR=Double.parseDouble(df.format(rs.getDouble(46)));//助攻率
+			    p.SR=Double.parseDouble(df.format(rs.getDouble(47)));//抢断率
+			    p.BR=Double.parseDouble(df.format(rs.getDouble(48)));//盖帽率
+			    p.TR=Double.parseDouble(df.format(rs.getDouble(49)));//失误率
+			    p.UseRate=Double.parseDouble(df.format(rs.getDouble(50)));//使用率(上场率)
+			    p.OR=Double.parseDouble(df.format(rs.getDouble(51)));//进攻率
+			    p.DR=Double.parseDouble(df.format(rs.getDouble(52)));//防守率
+			    p.WS=Double.parseDouble(df.format(rs.getDouble(53)));//WS
+			    p.O_WS=Double.parseDouble(df.format(rs.getDouble(54)));//进攻WS
+			    p.D_WS=Double.parseDouble(df.format(rs.getDouble(55)));//防守WS
+			    p.PER=Double.parseDouble(df.format(rs.getDouble(56)));//效率
+			    p.Dunk=Double.parseDouble(df.format(rs.getDouble(57)));//扣篮个数
+			    p.TTPO=Double.parseDouble(df.format(rs.getDouble(58)));//2/3+1(并不知道是什么)
+			    p.Blocked=Double.parseDouble(df.format(rs.getDouble(59)));//被冒个数
+			    p.ShootDistance=Double.parseDouble(df.format(rs.getDouble(60)));//投篮距离	    
+			    p.LXFGP=Double.parseDouble(df.format(rs.getDouble(61)*100));//LX表示篮下
+			    p.LXFGZ=Double.parseDouble(df.format(rs.getDouble(62)));
+			    p.LXFG=Double.parseDouble(df.format(rs.getDouble(63)));
+			    p.LXFGRate=Double.parseDouble(df.format(rs.getDouble(64)));//表示篮下投篮在所有投篮中比例，以下类推 
+			    p.JJLFGP=Double.parseDouble(df.format(rs.getDouble(65)*100));//JJ表示近距离
+			    p.JJLFGZ=Double.parseDouble(df.format(rs.getDouble(66)));
+			    p.JJLFG=Double.parseDouble(df.format(rs.getDouble(67)));
+			    p.JJLFGRate=Double.parseDouble(df.format(rs.getDouble(68)));
+			    p.ZJLFGP=Double.parseDouble(df.format(rs.getDouble(69)*100));//ZJL表示中距离
+			    p.ZJLFGZ=Double.parseDouble(df.format(rs.getDouble(70)));
+			    p.ZJLFG=Double.parseDouble(df.format(rs.getDouble(71)));
+			    p.ZJLFGRate=Double.parseDouble(df.format(rs.getDouble(72)));
+			    p.YJLFGP=Double.parseDouble(df.format(rs.getDouble(73)*100));//YJL表示远距离
+			    p.YJLFGZ=Double.parseDouble(df.format(rs.getDouble(74)));
+			    p.YJLFG=Double.parseDouble(df.format(rs.getDouble(75)));
+			    p.YJLFGRate=Double.parseDouble(df.format(rs.getDouble(76)));
+			    p.RealShootRate=Double.parseDouble(df.format(rs.getDouble(77)));//真实命中率
+			    p.S_RR=Double.parseDouble(df.format(rs.getDouble(78)));//投篮效率
+			    p.Htime=Double.parseDouble(df.format(rs.getDouble(79)));
+			    p.HFGP=Double.parseDouble(df.format(rs.getDouble(80)*100));
+			    p.HFGZ=Double.parseDouble(df.format(rs.getDouble(81)));//Z代表命中
+			    p.HFG=Double.parseDouble(df.format(rs.getDouble(82)));
+			    p.HSFGP=Double.parseDouble(df.format(rs.getDouble(83)*100));
+			    p.HSFGZ=Double.parseDouble(df.format(rs.getDouble(84)));
+			    p.HSFG=Double.parseDouble(df.format(rs.getDouble(85)));
+			    p.HFTGP=Double.parseDouble(df.format(rs.getDouble(86)*100));
+			    p.HFTGZ=Double.parseDouble(df.format(rs.getDouble(87)));
+			    p.HFTG=Double.parseDouble(df.format(rs.getDouble(88)));
+			    p.HRebounds=Double.parseDouble(df.format(rs.getDouble(89)));
+			    p.HORebouns=Double.parseDouble(df.format(rs.getDouble(90)));
+			    p.HDRebounds=Double.parseDouble(df.format(rs.getDouble(91)));
+			    p.HAssists=Double.parseDouble(df.format(rs.getDouble(92)));
+			    p.HSteals=Double.parseDouble(df.format(rs.getDouble(93)));
+			    p.HBlocks=Double.parseDouble(df.format(rs.getDouble(94)));
+			    p.HTurnovers=Double.parseDouble(df.format(rs.getDouble(95)));
+			    p.HFeals=Double.parseDouble(df.format(rs.getDouble(96)));
+			    p.HScores=Double.parseDouble(df.format(rs.getDouble(97)));
+			    p.salary=rs.getString(98);//薪水，带单位，所以用String
+			    p.isplayoff=rs.getBoolean(99);//是否是季候赛，是季后赛表示为1，不是为0 
+			    p.name=rs.getString(100);
+			    System.out.println(p.teamname);
+			    ps.add(p);
+    		}
+    		
+    		for(PlayerdatainfoVO temp : ps){
+    			PlayerVO p =  this.datainfotovo(temp);
+    			result.add(p);
+    		}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 		return result;
 	}
       
