a068d9d3382ff0188dd9e31a41c378206c2920c1
131250105 <465033064@qq.com>
2015-06-13 21:51:45 +0800

    .

--- src/thirdservice/playerControllerThirdService.java
+++ src/thirdservice/playerControllerThirdService.java
@@ -16,11 +16,12 @@ public interface playerControllerThirdService {
     public ArrayList<PlayerBasicInfoVO> getPlayersbyChar(char temp);
     public PlayerBasicInfoVO getPlayerbyname(String name);
     public PlayerVO getPlayervobyname(String name,String season);
+    public PlayerVO getPlayervobyid(String name,String season);
     public ArrayList<PlayerVO> getSeasonHotPlayers(String Season,DataType type,boolean ipl);
     public ArrayList<PlayerVO> getSeasonKingPlayers(String Season,DataType type,boolean ipl);
     public ArrayList<PlayerVO> getplayerbyteam(String tname,String Season);
     public double[] getdataforpiechart(String pid,String seasons,DataType dtp);
     public String getEnglishName(String id);
     public ArrayList<String> getseasonbyname(String name);
-    
+    public ArrayList<String> getthesameeason(String id1,String id2);
 }
