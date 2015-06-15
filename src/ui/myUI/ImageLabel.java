c2a7015fda300306bbea5afe0f9998ae4924e3b4
131250105 <465033064@qq.com>
2015-06-15 19:20:27 +0800

    .

--- src/ui/myUI/ImageLabel.java
+++ src/ui/myUI/ImageLabel.java
@@ -9,7 +9,7 @@ import javax.swing.JLabel;
 public class ImageLabel extends JLabel{
 	
 	public ImageLabel(ImageIcon image, int x, int y, int width, int height){				
-		image.setImage(image.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));		
+		//image.setImage(image.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT));		
 		this.setBounds(x, y, width, height);
 		this.setIcon(image);
 		this.setVisible(true);
