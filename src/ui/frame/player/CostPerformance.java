2252773a6bf70514d8dbc1a5edb0e8e45c879abf
131250208 <504757794@qq.com>
2015-06-15 15:45:56 +0800

    00

---
+++ src/ui/frame/player/CostPerformance.java
@@ -0,0 +1,26 @@
+package ui.frame.player;
+
+import java.awt.Color;
+
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+
+import ui.myUI.MyTable;
+
+public class CostPerformance extends JPanel {
+
+	private JComboBox<String> seasonComb;
+	private MyTable table;
+	public CostPerformance(){
+		this.setSize(1280, 720);
+		this.setLayout(null);
+		this.setBackground(Color.black);
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
